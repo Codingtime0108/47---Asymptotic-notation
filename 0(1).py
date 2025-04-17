2a9c75a42083262cfe3f71be89cf58f0490e29cc
Codingtime <13tia.vora@dulwich-seoul.kr>
2025-04-17 17:55:50 +0900

    0(1)

---
+++ 0(1).py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration=0
+    print("The total number entered by the user is", n)
+    iteration+=1
+    print("total iteration done by computer is",iteration,"\n")
+
+printnumber(10)
+printnumber(20)
+print("\n With any 'n' the time taken by the computer wil not change")
